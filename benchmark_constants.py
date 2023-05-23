19063afa75d800ae24b6d32f00b7895e6a531175
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-24 01:56:21 +0300

    leave just coding language in benchmark constants

--- benchmark_constants.py
+++ benchmark_constants.py
@@ -1,5 +1,3 @@
 CODING_LANGUAGES = ['rust', 'cpp', 'python-async', 'python-multithreaded']
 CODING_LANGUAGE = 'rust'
 assert CODING_LANGUAGE in CODING_LANGUAGES
-
-REQUEST_FILES = ['data/requests/1b.json', 'data/requests/256b.json', 'data/requests/1kb.json', 'data/requests/256kb.json', 'data/requests/1mb.json']
