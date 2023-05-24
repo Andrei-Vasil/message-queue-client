fc43eece0ad16699b8207b97c8d7ef803b4718a8
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-24 12:12:50 +0300

    updated http client to send scenario id to the message queues for benchmarking

--- http_client/publish.py
+++ http_client/publish.py
@@ -3,11 +3,11 @@ import os
 import time
 import requests
 
-def publish_multiple(topic: str, request_file: str, benchmark_file: str, no: int=1):
+def publish_multiple(topic: str, request_file: str, benchmark_file: str, scenario_id: str, no: int=1):
     for i in range(no):
-        publish(topic, request_file, benchmark_file, i)
+        publish(topic, request_file, benchmark_file, scenario_id, i)
 
-def publish(topic: str, request_file: str, benchmark_file: str, id: int):
+def publish(topic: str, request_file: str, benchmark_file: str, scenario_id: str, id: int):
     with open(request_file, 'r+') as f:
         data = json.load(f)
         data['benchmark_id'] = id
@@ -17,5 +17,5 @@ def publish(topic: str, request_file: str, benchmark_file: str, id: int):
 
     start = time.time()
     with open(request_file, "rb") as f:
-        requests.post(f'http://localhost:5000/publish/{topic}', headers={'Content-Type': 'application/json'}, data=f)
+        requests.post(f'http://localhost:5000/publish/{topic}/{scenario_id}', headers={'Content-Type': 'application/json'}, data=f)
     os.system(f'echo {start},{id} >> data/benchmarks/publish/{benchmark_file}')
--- http_client/receive.py
+++ http_client/receive.py
@@ -1,13 +1,18 @@
 import os
 import time
 
-def receive_multiple(client_id: int, topic: str, benchmark_file: str, no: int=1):
-    for _ in range(no):
-        receive(client_id, topic, benchmark_file)
+from http_client.misc import subscribe, unsubscribe
 
-def receive(client_id: int, topic: str, benchmark_file: str,):
+def receive_multiple(client_id: int, topic: str, benchmark_file: str, scenario_id: str, no: int=1):
+    client_id = subscribe(topic)
+    for _ in range(no):
+        receive(client_id, topic, benchmark_file, scenario_id)
+    unsubscribe(topic, client_id)
+
+
+def receive(client_id: int, topic: str, benchmark_file: str, scenario_id: str):
     start = time.time()
-    os.system(f'curl -s localhost:5000/subscription/{topic}/{client_id} -X GET >> /dev/null')
+    os.system(f'curl -s localhost:5000/subscription/{topic}/{client_id}/{scenario_id} -X GET >> /dev/null')
     end = time.time()
     delta = end - start
     os.system(f'echo {delta} >> data/benchmarks/receive/{benchmark_file}')
